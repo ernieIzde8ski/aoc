12e2edb61d091c64fdfa1219a78865c0c9e7a5b3
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2023-11-18 20:07:12 -0500

    Add /d06/

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,10 +1,16 @@
 #[macro_export]
 macro_rules! read_input {
     () => {
-        $crate::read_input!(impl format!("{}/input.txt", env!("CARGO_MANIFEST_DIR")), "expected input.txt")
+        $crate::read_input!(impl match cfg!(debug_assertions) {
+            true => {
+                eprintln!("Reading sample input!");
+                format!("{}/input.sample.txt", env!("CARGO_MANIFEST_DIR"))
+            },
+            false => format!("{}/input.txt", env!("CARGO_MANIFEST_DIR")),
+        }, "couldn't open input.txt!")
     };
     ($s:expr) => {
-        $crate::read_input!(impl $s, "expected an input file")
+        $crate::read_input!(impl $s, "couldn't open input file!")
     };
     (impl $s:expr, $m:literal) => {
         std::fs::read_to_string($s).expect($m)
@@ -17,14 +23,14 @@ macro_rules! benchmark {
         $crate::benchmark!(10_000, $code);
     };
 
-    ($loops:literal, $code:expr ) => {{
+    ($loops:literal, $code:expr) => {{
         let before = std::time::Instant::now();
         for _ in 0..($loops) {
             $code;
         }
         let elapsed = before.elapsed();
-        println!("Iterations:   {}", $loops);
-        println!("Time elapsed: {:.3?}", elapsed);
-        println!("Average time: {:.3?}", elapsed / $loops);
+        eprintln!("Iterations:   {}", $loops);
+        eprintln!("Time elapsed: {:.3?}", elapsed);
+        eprintln!("Average time: {:.3?}", elapsed / $loops);
     }};
 }
